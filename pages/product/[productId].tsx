e7adb9fbb119893c889df9534cb6b3ac9bda95f4
MarlonCosta <mrlnrnn@gmail.com>
2022-11-17 18:15:07 -0300

    Basic routing

---
+++ pages/product/[productId].tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+
+export default function ProductDetail() {
+    const router = useRouter()
+    const productId = router.query.productId
+    return <h1>
+        Details about product {productId}
+    </h1>
+}
